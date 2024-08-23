a56b1d6f781370f7ea6c2e0360c66c0eda3ee130
sti <86592209+sti-233@users.noreply.github.com>
2024-08-23 11:23:41 +0800

    add: custom_update_source page

--- patches/src/main/kotlin/app/revanced/patches/bilibili/misc/settings/patch/SettingsResourcePatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/bilibili/misc/settings/patch/SettingsResourcePatch.kt
@@ -27,6 +27,7 @@ object SettingsResourcePatch : ResourcePatch() {
     val isHd get() = packageName == "tv.danmaku.bilibilihd"
 
     private val xmls = arrayOf(
+        "biliroaming_setting_custom_update_source.xml",
         "biliroaming_locales_config.xml",
         "biliroaming_search_result.xml",
         "biliroaming_settings.xml",
