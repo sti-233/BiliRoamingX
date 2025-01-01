706af22b2c7801e4d85d8e35ff619d04ceaa9164
sti <86592209+sti-233@users.noreply.github.com>
2025-01-01 20:26:14 +0800

    test: fix Horizontal screen crash [skip ci]

---
+++ patches/src/main/kotlin/app/revanced/patches/bilibili/video/player/patch/PlayerCrashFixPatch.kt
@@ -0,0 +1,16 @@
+package app.revanced.patches.bilibili.video.player
+
+import app.revanced.patcher.data.Method
+import app.revanced.patcher.patch.Patch
+import app.revanced.patcher.patch.PatchContext
+import app.revanced.patches.bilibili.video.player.fingerprints.W2MethodFingerprint
+
+class W2MethodPatch : Patch {
+    override fun onApply(context: PatchContext) {
+        val method = context.findMethod(W2MethodFingerprint)
+            ?: throw IllegalStateException("Failed to find the method 'w2'.")
+        val originalCode = method.instructions
+        method.instructions.clear()
+        method.instructions.addAll(originalCode)
+    }
+}
