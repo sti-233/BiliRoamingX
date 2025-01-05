c51a3f4d5f3f1cb79b7ddd883a9891cbcf8ed4ae
sti <86592209+sti-233@users.noreply.github.com>
2025-01-05 15:53:44 +0800

    del: uncorrect file name

--- patches/src/main/kotlin/app/revanced/patches/bilibili/video/player/fingerprints/PlaylistMiniUGCPlayerServiceFingerprint.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.bilibili.video.player.fingerprints
-
-import app.revanced.patcher.fingerprint.MethodFingerprint
-
-object PlaylistMiniPlayerServiceFingerprint : MethodFingerprint(
-    strings = listOf("theseus-playlist", "tryStartMiniPlayerPlay, ", "PlaylistUGCMiniPlayerService"),
-    parameters = listOf("Lcom/bilibili/ship/theseus/united/page/miniplayer/DetailMiniPlayerService$b;"),
-    returnType = "V"
-)
