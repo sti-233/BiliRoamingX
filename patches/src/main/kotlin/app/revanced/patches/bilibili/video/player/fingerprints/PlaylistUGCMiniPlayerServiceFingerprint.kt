75680c6597b4b7eb6b8790465451fb8bbecd8de3
sti <86592209+sti-233@users.noreply.github.com>
2025-01-05 15:51:59 +0800

    fix: object name

--- patches/src/main/kotlin/app/revanced/patches/bilibili/video/player/fingerprints/PlaylistUGCMiniPlayerServiceFingerprint.kt
+++ patches/src/main/kotlin/app/revanced/patches/bilibili/video/player/fingerprints/PlaylistUGCMiniPlayerServiceFingerprint.kt
@@ -2,8 +2,8 @@ package app.revanced.patches.bilibili.video.player.fingerprints
 
 import app.revanced.patcher.fingerprint.MethodFingerprint
 
-object PlaylistOGVMiniPlayerServiceFingerprint : MethodFingerprint(
-    strings = listOf("theseus-playlist", "tryStartMiniPlayerPlay, ", "PlaylistOGVMiniPlayerService"),
+object PlaylistUGCMiniPlayerServiceFingerprint : MethodFingerprint(
+    strings = listOf("theseus-playlist", "tryStartMiniPlayerPlay, ", "PlaylistUGCMiniPlayerService"),
     parameters = listOf("Lcom/bilibili/ship/theseus/united/page/miniplayer/DetailMiniPlayerService$b;"),
     returnType = "V"
 )
